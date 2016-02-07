45c9965224b34d23100a6336d1d50f42d2943825
sekou.diarra <dev.diarra@gmail.com>
2016-02-07 22:41:04 +0100

    init repo

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTrainingApp
+ * @description
+ * # angularTrainingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTrainingApp', [
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
